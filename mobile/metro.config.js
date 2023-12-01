34b494e63c9b76ab64437dbd6cb087829e2da53e
Tan Le <tanlethanh8266@gmail.com>
2023-12-01 19:55:10 +0700

    feat: config mobile monorepo

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,4 +1,6 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require("path");
+
 
 /**
  * Metro configuration
@@ -6,6 +8,13 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+   watchFolders: [
+      path.resolve(__dirname, '../node_modules'),
+   ],
+   resolver: {
+      unstable_enableSymlinks: true
+   }
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
