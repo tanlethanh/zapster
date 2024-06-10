8ec12593bf30a8e838d91a42264fa671addd9de5
Tan Le <tanlethanh8266@gmail.com>
2024-06-10 10:15:26 +0700

    feat: simple mounting animation

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,31 @@
+import { MotionH1, MotionP } from '@/components/motion';
+
 export default function Home() {
 	return (
 		<main className="flex flex-col items-center justify-center h-screen gap-4">
-			<h1 className="text-5xl font-bold">Zapster</h1>
-			<p className="text-xl font-light">Open source team workspace</p>
-			<p className="text-base font-light">
+			<MotionH1
+				className="text-5xl font-bold"
+				initial={{ y: -100, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+			>
+				Zapster
+			</MotionH1>
+			<MotionP
+				className="text-xl font-light"
+				initial={{ y: -30, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				transition={{ delay: 0.3 }}
+			>
+				Open source team workspace
+			</MotionP>
+			<MotionP
+				className="text-base font-light"
+				initial={{ y: -10, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				transition={{ delay: 0.6 }}
+			>
 				Notion, Google Calendar, Discord, Github,...
-			</p>
+			</MotionP>
 		</main>
 	);
 }
---
+++ web/src/components/motion/index.tsx
@@ -0,0 +1,5 @@
+'use client';
+import { motion } from 'framer-motion';
+
+export const MotionH1 = motion.h1;
+export const MotionP = motion.p;
